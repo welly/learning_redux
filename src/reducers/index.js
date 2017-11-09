dbace0a874451f18c2c5ecfed769d60f119d9eab
Alastair Moore <welly@protonmail.com>
2017-11-09 00:14:28 +0000

    Added activebooks reducer to stack

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import ActiveBook from './reducer_activebook';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
+	activeBook: ActiveBook,
 	books: BooksReducer
 });
 
