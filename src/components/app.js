87752e029bcba1328f74649960316a6fa6ce4b41
Alastair Moore <welly@protonmail.com>
2017-11-09 00:14:44 +0000

    Rendering bookdetail

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import BookDetail from '../containers/book_detail';
 import BookList from '../containers/book_list';
 
 export default class App extends Component {
@@ -7,6 +8,7 @@ export default class App extends Component {
     return (
 		<div>
 			<BookList />
+			<BookDetail />
 		</div>
     );
   }
